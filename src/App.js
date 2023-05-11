6398532d0ddbb2cd484c318a9d1d1df59ecb21be
Aneta <sopaaneta@gmail.com>
2023-05-11 16:25:55 +0200

    Add functionality remove and add quest after click the button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.module.scss';
-import { useState } from 'react';
-
-const people = [
-  { id: 1, name: 'Ala', age: 34 },
-  { id: 2, name: 'Kris', age: 14 },
-];
+import { useEffect, useState } from 'react';
 
 export default function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   // state for the input data
   const [checkedGuest, setCheckedGuest] = useState(false);
-  const [guests, setGuests] = useState(people);
+  const [guests, setGuests] = useState({});
+
+  const baseUrl = 'http://localhost:4000';
+  const response = fetch(`${baseUrl}/guests/:id`);
+  const guest = response.json();
+  console.log({ guest });
+
+  // trigger an action on first render
+  // useEffect(() => {
+  //   async function fetchQuests() {
+  //     const response = await fetch(`${baseUrl}/guests/:id`);
+  //     const guest = await response.json();
+  //     console.log({ guest });
+
+  //     setGuests(guest);
+  //   }
+  //   fetchQuests()
+  //     .then()
+  //     .catch((error) => console.log(error));
+  // }, [guests]);
 
   return (
     <>
@@ -26,6 +40,7 @@ export default function App() {
               value={firstName}
             />
             <input
+              aria-label="attending"
               checked={checkedGuest}
               type="checkbox"
               // update the state value with the event.currentTarget.checked
@@ -42,6 +57,7 @@ export default function App() {
               value={lastName}
             />
             <input
+              aria-label="attending"
               checked={checkedGuest}
               type="checkbox"
               // update the state value with the event.currentTarget.checked
@@ -54,25 +70,25 @@ export default function App() {
         </form>
       </div>
       <div>
-        {guests.map((user) => {
+        {/* {guests.map((user) => {
           return (
             <div key={`user-${user.id}`}>
               <span>{user.name}</span>
               <span>{user.age}</span>
             </div>
           );
-        })}
-        <button
+        })} */}
+        <button onClick={async () => await setGuests()}>Get guest</button>
+        {/* <button
           onClick={() => {
             // variable for the new quest
             const newPerson = { id: 3, name: 'Alex', age: 89 };
 
             // create copy of the current state
-            const newQuest = [...people];
+            const newQuest = [...guests];
 
             // update the copy created on step 1
             newQuest.push(newPerson);
-            console.log(people);
 
             // set state to the copy of the old state
             setGuests(newQuest);
@@ -80,6 +96,20 @@ export default function App() {
         >
           Add new quest
         </button>
+        <button
+          onClick={() => {
+            // create a copy
+            const newPerson = [...guests];
+
+            // update the value
+            newPerson.length = newPerson.length - 1;
+
+            // set new state
+            setGuests(newPerson);
+          }}
+        >
+          Remove
+        </button> */}
       </div>
     </>
   );
