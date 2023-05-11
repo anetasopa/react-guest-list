b72427dcb9a13b073550491cc54bbac2a4e3e79e
Aneta <sopaaneta@gmail.com>
2023-05-11 23:20:59 +0200

    Add fetch to GET, POST, PUT and DELETE the data

--- src/App.js
+++ src/App.js
@@ -1,116 +1,156 @@
 import './App.module.scss';
 import { useEffect, useState } from 'react';
 
+const baseUrl = 'http://localhost:4000';
+
 export default function App() {
+  const [guests, setGuests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  // state for the input data
-  const [checkedGuest, setCheckedGuest] = useState(false);
-  const [guests, setGuests] = useState({});
-
-  const baseUrl = 'http://localhost:4000';
-  const response = fetch(`${baseUrl}/guests/:id`);
-  const guest = response.json();
-  console.log({ guest });
 
   // trigger an action on first render
-  // useEffect(() => {
-  //   async function fetchQuests() {
-  //     const response = await fetch(`${baseUrl}/guests/:id`);
-  //     const guest = await response.json();
-  //     console.log({ guest });
+  // get data
+  useEffect(() => {
+    async function fetchQuests() {
+      const response = await fetch(`${baseUrl}/guests/`);
+      const guest = await response.json();
+      console.log({ guest });
 
-  //     setGuests(guest);
-  //   }
-  //   fetchQuests()
-  //     .then()
-  //     .catch((error) => console.log(error));
-  // }, [guests]);
+      setGuests(guest);
+      setIsLoading(false);
+    }
+    fetchQuests()
+      .then()
+      .catch((error) => console.log(error));
+  }, [guests]);
+
+  // add guest
+  const addGuest = async () => {
+    fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        // default attending
+        attending: false,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // clear the value in the inputs
+        setFirstName('');
+        setLastName('');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  // delete guest
+  const deleteGuest = async (guestId) => {
+    fetch(`${baseUrl}/guests/${guestId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  };
+
+  // update attending
+  const toggleAttending = async (guestId) => {
+    const guestToUpdate = guests.find((guest) => guest.id === guestId);
+    const updatedGuest = {
+      ...guestToUpdate,
+      attending: !guestToUpdate.attending,
+    };
+
+    return fetch(`${baseUrl}/guests/${guestId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: true }),
+    })
+      .then((data) => {
+        console.log(data); // do something with the data if needed
+        return updatedGuest;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        return guestToUpdate;
+      });
+  };
+
+  useEffect(() => {
+    if (guests.length > 0) {
+      setIsLoading(false);
+    }
+  }),
+    [guests];
 
   return (
-    <>
-      <div data-test-id="guest">
-        <form onSubmit={(e) => e.preventDefault()}>
-          <label>
-            First name
+    <div>
+      <h1>Guest List</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <label htmlFor="firstName">First name</label>
             <input
-              // use the current value of the state as the checked property of the input
-              placeholder="First name"
-              onChange={(e) => setFirstName(e.currentTarget.value)}
+              id="firstName"
+              name="firstName"
               value={firstName}
+              onChange={(event) => setFirstName(event.currentTarget.value)}
             />
+            <br />
+            <label htmlFor="lastName">Last name</label>
             <input
-              aria-label="attending"
-              checked={checkedGuest}
-              type="checkbox"
-              // update the state value with the event.currentTarget.checked
-              onChange={(e) => {
-                setCheckedGuest(e.currentTarget.checked);
-              }}
-            />
-          </label>
-          <label>
-            Last name
-            <input
-              placeholder="Last name"
-              onChange={(e) => setLastName(e.currentTarget.value)}
+              id="lastName"
+              name="lastName"
               value={lastName}
+              onChange={(event) => setLastName(event.currentTarget.value)}
             />
-            <input
-              aria-label="attending"
-              checked={checkedGuest}
-              type="checkbox"
-              // update the state value with the event.currentTarget.checked
-              onChange={(e) => {
-                setCheckedGuest(e.currentTarget.checked);
-              }}
-            />
-          </label>
-          <button>Return</button>
-        </form>
-      </div>
-      <div>
-        {/* {guests.map((user) => {
-          return (
-            <div key={`user-${user.id}`}>
-              <span>{user.name}</span>
-              <span>{user.age}</span>
-            </div>
-          );
-        })} */}
-        <button onClick={async () => await setGuests()}>Get guest</button>
-        {/* <button
-          onClick={() => {
-            // variable for the new quest
-            const newPerson = { id: 3, name: 'Alex', age: 89 };
-
-            // create copy of the current state
-            const newQuest = [...guests];
-
-            // update the copy created on step 1
-            newQuest.push(newPerson);
-
-            // set state to the copy of the old state
-            setGuests(newQuest);
-          }}
-        >
-          Add new quest
-        </button>
-        <button
-          onClick={() => {
-            // create a copy
-            const newPerson = [...guests];
-
-            // update the value
-            newPerson.length = newPerson.length - 1;
-
-            // set new state
-            setGuests(newPerson);
-          }}
-        >
-          Remove
-        </button> */}
-      </div>
-    </>
+            <br />
+            <button onClick={() => addGuest()}>Add guest</button>
+          </form>
+          <ul>
+            {guests.map((guest) => (
+              // eslint-disable-next-line react/jsx-key
+              <>
+                <li key={guest.id} data-test-id="guest">
+                  <label htmlFor={`attending-${guest.id}`}>Attending:</label>
+                  <input
+                    type="checkbox"
+                    id={`attending-${guest.id}`}
+                    aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+                    checked={guest.attending}
+                    onChange={() => toggleAttending(guest.id)}
+                  />
+                  {`${guest.firstName} ${guest.lastName}`}
+                </li>
+                <button
+                  aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+                  onClick={() => deleteGuest(guest.id)}
+                >
+                  Remove
+                </button>
+              </>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
   );
 }
