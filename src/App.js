a4702e7bfbe9607757c90ec105600fb3981b171b
Aneta <sopaaneta@gmail.com>
2023-05-12 18:33:44 +0200

    Fix some errors from drone

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
 
   // add guest
   const addGuest = async () => {
-    await fetch(`${baseUrl}/guests`, {
+    const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -50,57 +50,37 @@ export default function App() {
       body: JSON.stringify({
         firstName: firstName,
         lastName: lastName,
-        // default attending
-        attending: false,
       }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // clear the value in the inputs
-        setFirstName('');
-        setLastName('');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    });
+    const createdGuest = await response.json();
+    const newGuestList = [...guests, createdGuest];
+    setGuests(newGuestList);
   };
 
   // delete guest
-  const deleteGuest = async (guestId) => {
-    await fetch(`${baseUrl}/guests/${guestId}`, {
+  const deleteGuest = async (para) => {
+    const response = await fetch(`${baseUrl}/guests/${para}`, {
       method: 'DELETE',
-    })
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
+    });
+    const deletedGuest = await response.json();
+    const newGuestList = guests.filter((guest) => guest.id !== deletedGuest.id);
+    setGuests(newGuestList);
   };
 
   // update attending
-  const toggleAttending = async (guestId) => {
-    const guestToUpdate = guests.find((guest) => guest.id === guestId);
-    const updatedGuest = {
-      ...guestToUpdate,
-      attending: !guestToUpdate.attending,
-    };
-
-    return await fetch(`${baseUrl}/guests/${guestId}`, {
+  const toggleAttending = async (para) => {
+    const index = guests.findIndex((guest) => guest.id === para);
+    const response = await fetch(`${baseUrl}/guests/${para}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ attending: updatedGuest.attending }),
-    })
-      .then((data) => {
-        console.log(data); // do something with the data if needed
-        return updatedGuest;
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        return guestToUpdate;
-      });
+      body: JSON.stringify({ attending: !guests[index].attending }),
+    });
+    const updatedGuest = await response.json();
+    const newUpdatedGuest = [...guests];
+    newUpdatedGuest[index].attending = updatedGuest.attending;
+    setGuests(newUpdatedGuest);
   };
 
   return (
@@ -133,35 +113,32 @@ export default function App() {
           <br />
           <button onClick={() => addGuest()}>Add guest</button>
         </form>
-        <ul>
-          {guests.map((guest) => (
-            <div key={`user-${guest.id}`} data-test-id="guest">
-              <li>
-                <FontAwesomeIcon
-                  className={styles.icon}
-                  icon={faLeaf}
-                  style={{ color: '#ffffff' }}
-                />
-                <label htmlFor={`attending-${guest.id}`}>Attending:</label>
-                <input
-                  type="checkbox"
-                  id={`attending-${guest.id}`}
-                  aria-label={`${guest.firstName} ${guest.lastName} attending status`}
-                  checked={guest.attending}
-                  onChange={() => toggleAttending(guest.id)}
-                />
-                <span>{`${guest.firstName} ${guest.lastName}`}</span>
-              </li>
-              <button
-                className={styles.buttonRemove}
-                aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
-                onClick={() => deleteGuest(guest.id)}
-              >
-                Remove
-              </button>
-            </div>
-          ))}
-        </ul>
+        {guests.map((guest) => (
+          <div key={`user-${guest.id}`} data-test-id="guest">
+            <FontAwesomeIcon
+              className={styles.icon}
+              icon={faLeaf}
+              style={{ color: '#ffffff' }}
+            />
+            <label htmlFor={`attending-${guest.id}`}>Attending:</label>
+            <input
+              type="checkbox"
+              id={`attending-${guest.id}`}
+              aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+              checked={guest.attending}
+              onChange={() => toggleAttending(guest.id)}
+            />
+            <span>{`${guest.firstName} ${guest.lastName}`}</span>
+
+            <button
+              className={styles.buttonRemove}
+              aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+              onClick={() => deleteGuest(guest.id)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
       </div>
     </>
   );
