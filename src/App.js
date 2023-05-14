418af218d1846a338fd4abd484f9a4668abb3f9a
Aneta <sopaaneta@gmail.com>
2023-05-14 22:17:09 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { faLeaf, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import image from './images/list.png';
 
-//const baseUrl = 'http://localhost:4000';
+// const baseUrl = 'http://localhost:4000';
 const baseUrl = 'https://c2307a9a-e779-4389-8c23-48c4e3611827.id.repl.co';
 
 export default function App() {
@@ -11,8 +12,6 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-
-  //useEffect(() => console.log('debug ' + isLoading), [isLoading]);
 
   useEffect(() => {
     setIsLoading(false);
@@ -26,10 +25,10 @@ export default function App() {
 
       const response = await fetch(`${baseUrl}/guests/`);
       const guest = await response.json();
-      //await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
 
       setGuests([...guest]);
-      //setIsLoading(false);
     }
     fetchQuests().catch((error) => console.log(error));
   }, []);
@@ -82,45 +81,61 @@ export default function App() {
   const renderList = () => {
     return guests.map((guest) => (
       <div key={`user-${guest.id}`} data-test-id="guest">
-        <FontAwesomeIcon
-          className={styles.icon}
-          icon={faLeaf}
-          style={{ color: '#ffffff' }}
-        />
-        <label htmlFor={`attending-${guest.id}`}>Attending:</label>
+        <label
+          className={styles.attendingLabel}
+          htmlFor={`attending-${guest.id}`}
+        >
+          Attending:
+        </label>
         <input
+          className={styles.attendingCheckbox}
           type="checkbox"
           id={`attending-${guest.id}`}
           aria-label={`${guest.firstName} ${guest.lastName} attending status`}
           checked={guest.attending}
           onChange={() => toggleAttending(guest.id)}
         />
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={faLeaf}
+          style={guest.attending ? { color: '#3D402C' } : { color: 'white' }}
+        />
         <span>{`${guest.firstName} ${guest.lastName}`}</span>
-        <button
+        <FontAwesomeIcon
+          className={styles.close}
+          style={{ color: '#D96690' }}
+          icon={faXmark}
+          aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+          onClick={() => deleteGuest(guest.id)}
+        />
+        {/* <button
           className={styles.buttonRemove}
           aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
           onClick={() => deleteGuest(guest.id)}
         >
           Remove
-        </button>
+        </button> */}
       </div>
     ));
   };
 
   return (
     <>
-      {/* <img className={styles.img} src={image} alt="img" /> */}
-      <div className={styles.container}>
+      <img className={styles.img} src={image} alt="img" />
+
+      <div className={styles.containerForm}>
         <h1>Guest List</h1>
         {JSON.stringify(toggleAttending)}
         <form onSubmit={(event) => event.preventDefault()}>
-          <label htmlFor="firstName">First name</label>
-          <input
-            id="firstName"
-            disabled={isLoading}
-            value={firstName}
-            onChange={(event) => setFirstName(event.currentTarget.value)}
-          />
+          <div className={styles.floatLabelField}>
+            <label htmlFor="firstName">First name</label>
+            <input
+              id="firstName"
+              disabled={isLoading}
+              value={firstName}
+              onChange={(event) => setFirstName(event.currentTarget.value)}
+            />
+          </div>
           <br />
           <label htmlFor="lastName">Last name</label>
           <input
