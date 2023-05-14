042b0d337763723339bc30b9048700f5decfd7f3
Aneta <sopaaneta@gmail.com>
2023-05-14 17:02:44 +0200

    adjusting to pass e2e drone tests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import { faLeaf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
-import image from '../src/images/guest-list.png';
 import styles from './App.module.scss';
 
 const baseUrl = 'http://localhost:4000';
 
 export default function App() {
   const [guests, setGuests] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
-  useEffect(() => console.log('debug ' + isLoading), [isLoading]);
+  //useEffect(() => console.log('debug ' + isLoading), [isLoading]);
 
-  useEffect(() => {
-    setIsLoading(false);
-  }, [guests]);
+  // useEffect(() => {
+  //   setIsLoading(false);
+  // }, [guests]);
 
   // trigger an action on first render
   // get data
@@ -26,8 +25,10 @@ export default function App() {
 
       const response = await fetch(`${baseUrl}/guests/`);
       const guest = await response.json();
+      //await new Promise((resolve) => setTimeout(resolve, 1000));
 
       setGuests([...guest]);
+      setIsLoading(false);
     }
     fetchQuests().catch((error) => console.log(error));
   }, []);
@@ -75,12 +76,39 @@ export default function App() {
     setGuests(newUpdatedGuest);
   };
 
+  const renderList = () => {
+    return guests.map((guest) => (
+      <div key={`user-${guest.id}`} data-test-id="guest">
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={faLeaf}
+          style={{ color: '#ffffff' }}
+        />
+        <label htmlFor={`attending-${guest.id}`}>Attending:</label>
+        <input
+          type="checkbox"
+          id={`attending-${guest.id}`}
+          aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+          checked={guest.attending}
+          onChange={() => toggleAttending(guest.id)}
+        />
+        <span>{`${guest.firstName} ${guest.lastName}`}</span>
+        <button
+          className={styles.buttonRemove}
+          aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+          onClick={() => deleteGuest(guest.id)}
+        >
+          Remove
+        </button>
+      </div>
+    ));
+  };
+
   return (
     <>
-      <img className={styles.img} src={image} alt="img" />
+      {/* <img className={styles.img} src={image} alt="img" /> */}
       <div className={styles.container}>
         <h1>Guest List</h1>
-        {isLoading ? <div>Loading...</div> : ''}
         {JSON.stringify(toggleAttending)}
         <form
           onSubmit={(event) => {
@@ -88,64 +116,26 @@ export default function App() {
           }}
         >
           <label htmlFor="firstName">First name</label>
-          {isLoading ? (
-            <input
-              id="firstName"
-              disabled
-              value={firstName}
-              onChange={(event) => setFirstName(event.currentTarget.value)}
-            />
-          ) : (
-            <input
-              id="firstName"
-              value={firstName}
-              onChange={(event) => setFirstName(event.currentTarget.value)}
-            />
-          )}
+          <input
+            id="firstName"
+            disabled={isLoading}
+            value={firstName}
+            onChange={(event) => setFirstName(event.currentTarget.value)}
+          />
           <br />
           <label htmlFor="lastName">Last name</label>
-          {isLoading ? (
-            <input
-              disabled
-              id="lastName"
-              value={lastName}
-              onChange={(event) => setLastName(event.currentTarget.value)}
-            />
-          ) : (
-            <input
-              id="lastName"
-              value={lastName}
-              onChange={(event) => setLastName(event.currentTarget.value)}
-            />
-          )}
+          <input
+            disabled={isLoading}
+            id="lastName"
+            value={lastName}
+            onChange={(event) => setLastName(event.currentTarget.value)}
+          />
           <br />
-          <button onClick={() => addGuest()}>Add guest</button>
+          <button onClick={() => addGuest()} disabled={isLoading}>
+            Add guest
+          </button>
         </form>
-        {guests.map((guest) => (
-          <div key={`user-${guest.id}`} data-test-id="guest">
-            <FontAwesomeIcon
-              className={styles.icon}
-              icon={faLeaf}
-              style={{ color: '#ffffff' }}
-            />
-            <label htmlFor={`attending-${guest.id}`}>Attending:</label>
-            <input
-              type="checkbox"
-              id={`attending-${guest.id}`}
-              aria-label={`${guest.firstName} ${guest.lastName} attending status`}
-              checked={guest.attending}
-              onChange={() => toggleAttending(guest.id)}
-            />
-            <span>{`${guest.firstName} ${guest.lastName}`}</span>
-            <button
-              className={styles.buttonRemove}
-              aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
-              onClick={() => deleteGuest(guest.id)}
-            >
-              Remove
-            </button>
-          </div>
-        ))}
+        {isLoading ? <div>Loading...</div> : renderList()}
       </div>
     </>
   );
